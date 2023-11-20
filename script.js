9cdd93ad3f61fd9c0a3b40361b1efb8918f0d0b5
Mahin-Abrar <mahinabrar25@gmail.com>
2023-11-20 12:58:02 +0600

    Bmi secured, UI not ready

---
+++ script.js
@@ -0,0 +1,46 @@
+let slider1 = document.getElementById("myRange1");
+let output1 = document.getElementById("demo1");
+output1.innerHTML = slider1.value;
+
+slider1.oninput = function() {
+  output1.innerHTML = this.value;
+  updateBMI();
+  statu();
+}
+
+let slider2 = document.getElementById("myRange2");
+let output2 = document.getElementById("demo2");
+output2.innerHTML = slider2.value;
+
+slider2.oninput = function() {
+  output2.innerHTML = this.value;
+  updateBMI();
+  statu();
+}
+
+let emoji=document.getElementById("demo4");
+let target=document.getElementById("demo3");
+function updateBMI() {
+    let weight = parseFloat(output1.innerHTML);
+    let height = parseFloat(output2.innerHTML);
+    let bmi = weight/(height*height);
+    target.innerHTML = bmi.toFixed(2);
+  }
+
+function statu(){
+    if (target.innerHTML<18.5){
+        emoji.innerHTML="Underweight 🤢";
+    }else if(18.5<=target.innerHTML && target.innerHTML<=24.9){
+        emoji.innerHTML="Normalweight 🥳";
+    }else if (25<=target.innerHTML && target.innerHTML<30){
+        emoji.innerHTML="Overweight 🍕";
+    }else{
+        emoji.innerHTML="Obese 🐘";
+    }
+}
+
+  
+//   Underweight: BMI is less than 18.5
+// Normal weight: BMI is 18.5 to 24.9
+// Overweight: BMI is 25 to 29.9
+// Obese: BMI is 30 or more
